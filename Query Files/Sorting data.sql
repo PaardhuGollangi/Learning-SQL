93d49a5e6a71671ff84a79386cf4c770f97e9381
Udit Kumar Chatterjee <quantumudit@gmail.com>
2019-12-02 16:09:10 +0530

    Comment Correction
    Commented out some of the non-query statement in the file.

--- Query Files/Sorting data.sql
+++ Query Files/Sorting data.sql
@@ -1,27 +1,29 @@
-
 use college;
 
-#Order By Clause
-#-----------------
+/*
+Order By Clause
+------------------
 
-#Used to sort the result from the select statement
-#--------------------------------------------------------
+Used to sort the result from the select statement
+--------------------------------------------------------
+*/
 
-#1. sort by ascending student name
+-- 1. sort by ascending student name
 
 select * from student;
 select * from student order by sname;
 select * from student order by sname desc;
 
-#2. sort by descending marks
+-- 2. sort by descending marks
 
 select * from student order by marks desc;
 
-#3. sort by name in ascending and marks in descending
+-- 3. sort by name in ascending and marks in descending
 
 select * from student order by sname asc, Marks desc;
 
-#4. custom sort
+-- 4. custom sort
+
 select * from faculty;
 select * from faculty order by field(qualification, "M.Tech", "Ph.D", "B.Tech");
 
