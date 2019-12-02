67fd114e9173297c280ced93394792f8ceebb5b9
Udit Kumar Chatterjee <quantumudit@gmail.com>
2019-12-02 17:36:28 +0530

    Formatting Changes
    Very minor typo-changes has been done.

--- Query Files/Sorting data.sql
+++ Query Files/Sorting data.sql
@@ -4,25 +4,24 @@ USE college;
 Order By Clause
 ------------------
 
-Used to sort the result from the select statement
---------------------------------------------------------
+Used to sort the result from the select statement.
 */
 
--- 1. sort by ascending student name
+-- 1. Sort by ascending student name
 
 SELECT * FROM student;
 SELECT * FROM student ORDER BY sname;
 SELECT * FROM student ORDER BY sname DESC;
 
--- 2. sort by descending marks
+-- 2. Sort by descending marks
 
 SELECT * FROM student ORDER BY marks DESC;
 
--- 3. sort by name in ascending and marks in descending
+-- 3. Sort by name in ascending and marks in descending
 
 SELECT * FROM student ORDER BY sname ASC, Marks DESC;
 
--- 4. custom sort
+-- 4. Custom Sort
 
 SELECT * FROM faculty;
 SELECT * FROM faculty ORDER BY field(qualification, "M.Tech", "Ph.D", "B.Tech");
