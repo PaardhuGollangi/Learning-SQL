4f2972bcbe377d264b809e1811aaffcc99998625
Udit Kumar Chatterjee <quantumudit@gmail.com>
2020-08-08 23:44:03 +0530

    Complete notes on data munging

--- PostgreSQL/Advanced_SQL_for_Data_Science/SQL_Queries/1.1_Data_Munging.sql
+++ PostgreSQL/Advanced_SQL_for_Data_Science/SQL_Queries/1.1_Data_Munging.sql
@@ -1,4 +1,4 @@
--- Reformatting Character Data --
+-- REFORMATTING CHARACTER DATA --
 -- =========================== --
 
 -- Viewing the distinct departments available:
@@ -49,20 +49,104 @@ SELECT
 FROM
 	staff;
 
--- Character Extraction & Replacements --
+-- CHARACTER EXTRACTION & REPLACEMENTS --
 -- =================================== --
 
+-- Extracting all the letters from 5th position onwards from a random string:
+
+SELECT
+    SUBSTRING('ABCDEFGHIJKL' FROM 5) test_string;
+
+-- Extracting all the letters from 1st position onwards upto 3 letters from a random string:
+
+SELECT
+    SUBSTRING('ABCDEFGHIJKL' FROM 1
+FOR 3) test_string;
+
+-- Replacing "Assistant" with "Asst." in "job_title"
+
+SELECT
+    OVERLAY(job_title PLACING
+'Asst.' FROM 1 FOR 9)
+FROM
+	staff
+WHERE
+	job_title LIKE 'Assistant%'
 
 
+-- FILTERING WITH REGULAR EXPRESSION --
+-- ================================== --
+
+-- Finding the "job_titles" that has word "Assistant" associated along with the rank "III" or, "IV":
+
+SELECT
+    job_title
+FROM
+    staff
+WHERE
+	job_title
+SIMILAR TO '%Assistant%(III|IV)'
+
+-- Finding the "job_titles" that has word "Assistant" associated along with any roman numeral rank greater than "I":
+
+SELECT
+    job_title
+FROM
+    staff
+WHERE
+	job_title
+SIMILAR TO '%Assistant% I_'
+
+-- Finding the "job_titles" that contains the letter "E", "P" or, "S" and any character afterwards:
+
+SELECT
+    job_title
+FROM
+    staff
+WHERE
+	job_title
+SIMILAR TO '[EPS]%'
 
 
-	
+-- REFORMATING NUMERICAL DATA --
+-- ========================== --
 
+-- Finding the average salary by department (trucated to 1 decimal places):
 
+SELECT
+    department,
+    TRUNC(AVG(salary),1)
+FROM
+    staff
+GROUP BY
+	department;
 
+-- Finding the average salary by department (rouding to 1 decimal places):
 
+SELECT
+    department,
+    ROUND(AVG(salary),1)
+FROM
+    staff
+GROUP BY
+	department;
 
+-- Finding the average salary by department (rouding-up to zero decimal places):
 
-	
+SELECT
+    department,
+    CEIL(AVG(salary))
+FROM
+    staff
+GROUP BY
+	department;
 
+-- Finding the average salary by department (rouding-down to zero decimal places):	
 
+SELECT
+    department,
+    FLOOR(AVG(salary))
+FROM
+    staff
+GROUP BY
+	department;
